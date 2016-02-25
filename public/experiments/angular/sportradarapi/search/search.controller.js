fc65d2b2836aea318fb906e42f75c83aa74a827a
Sujith Narayan <sujithrp@ccs.neu.edu>
2016-02-25 02:43:58 -0500

    server proxy working on localhost

--- public/experiments/angular/sportradarapi/search/search.controller.js
+++ public/experiments/angular/sportradarapi/search/search.controller.js
@@ -8,28 +8,15 @@
         .controller("SearchController", searchController);
 
     function searchController($scope, $http) {
-
-        var appId = ":ZT6UwwHmcPqetCdc6nO9+16rjz5iNa/9izs7sxGO1io";
         $scope.searchTeam = search;
-        var encoded = window.btoa(appId);
-        //$scope.team="AC Milan";
 
-        function search(team) {
-            var url = "http://localhost:1337/api.sportradar.us/nba-t3/teams/583ecd4f-fb46-11e1-82cb-f4ce4684ea4c/profile.json?api_key=9hx9mmdj93q7hz26yegm47tu";
-            //var url = "https://api.datamarket.azure.com/Bing/Search/News?Query=%27ac%20milan%27&$format=json";
-            $http({
-                method: 'GET',
-                url: url
-                //headers: {
-                  //  'Authorization': 'Basic ' + encoded
-                //}
-            })
-                .then(function(data) {
-                    console.log(data)
-                })
+        function search() {
+            var url = "/api/nba-t3/teams/583ecd4f-fb46-11e1-82cb-f4ce4684ea4c/profile.json?api_key=9hx9mmdj93q7hz26yegm47tu";
+            $http.get(url)
+                .then(function(response) {
+                console.log(response);
+            });
         }
-
-
     }
 
 })();
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var request = require('request');
 var app = express();
 app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-app.get('/sayHello', function(req, res){
-    res.send('hello world!');
+app.get('/api/*', function(req, res) {
+    var path = req.url.replace("/api","");
+    console.log(path);
+    var url = "http://api.sportradar.us" + path;
+    req.pipe(request(url)).pipe(res);
 });
 app.listen(port, ipaddress);
