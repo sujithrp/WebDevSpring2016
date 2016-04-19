b7fef1498439488861368226c411f44f131f39c0
Sujith Narayan <sujithrp@ccs.neu.edu>
2016-04-19 18:46:18 -0400

    minor fixes - 2

--- public/project/client/views/home/home.controller.js
+++ public/project/client/views/home/home.controller.js
@@ -6,74 +6,42 @@
         .module("SportsApp")
         .controller("HomeController", homeController);
 
-    function homeController($http,$scope,$rootScope,$location, CacheService) {
-        //var nbaFixtureUrl = "/sports/nba-t3/games/2016/04/05/schedule.json?api_key=9hx9mmdj93q7hz26yegm47tu";
-        //var nbaFixtureUrl = "/sports/nba-t3/games/2015/PST/schedule.json?api_key=9hx9mmdj93q7hz26yegm47tu";
-        //var nbaFixtureUrl = "/sports/nba-t3/teams/583ecd4f-fb46-11e1-82cb-f4ce4684ea4c/profile.json?api_key=9hx9mmdj93q7hz26yegm47tu";
-        //var nbaFixtureUrl = "/sports/nba-t3/teams/583ecd4f-fb46-11e1-82cb-f4ce4684ea4c/profile.json?api_key=9hx9mmdj93q7hz26yegm47tu";
-        //var nflFixtureUrl = "/sports/nfl-t1/2015/PST/schedule.json?api_key=c9gmk2hsnccg8hcwhmfyj9uj";
+    function homeController($http,$scope,$rootScope,$location, UserService, CacheService) {
+        UserService
+            .getCurrentUser()
+            .then(function (res) {
+                $rootScope.currentUser = res.data;
+                $scope.message = null;
+
+                $scope.user = $rootScope.currentUser;
+            });
         var nflFixtureUrl = "/sports/nfl-t1/2015/REG/4/boxscore.json?api_key=c9gmk2hsnccg8hcwhmfyj9uj";
-        //var nflFixtureUrl = "/sports/nfl-t1/teams/2015/REG/standings.json?api_key=c9gmk2hsnccg8hcwhmfyj9uj";
 
-        //var resultObj = {
-        //    "home": "home team fsdgfdsgsolnglng",
-        //    "away": "away team ffefbeklfbeufbh",
-        //    "homePoints": "45",
-        //    "awayPoints": "55"
-        //};
-        //$scope.resultsArr = [resultObj];
+        var resultObj = {
+            "home": "home team fsdgfdsgsolnglng",
+            "away": "away team ffefbeklfbeufbh",
+            "homePoints": "45",
+            "awayPoints": "55"
+        };
+        $scope.resultsArr = [resultObj];
 
-        //console.log($scope.resultsArr);
         //$http.get(nflFixtureUrl)
-        //    .then(function (response) {
+        //    .then(function(response) {
         //        $scope.resultsArr = [];
-        //        var gamesArr = [];
-        //        console.log(response);
-        //        var details = response.data;
-        //        console.log(details);
-        //        var weeksArr = details.weeks;
-        //        var weekNumber;
-        //        for (weekNumber in weeksArr) {
-        //            var weekDetails = weeksArr[weekNumber];
-        //            var gamesArr = weekDetails.games;
-        //            var gameNumber;
-        //            for (gameNumber in gamesArr) {
-        //                var gameObj = gamesArr[gameNumber];
-        //                CacheService.codeToName(gameObj.home).then(function(responseHome) {
-        //                    CacheService.codeToName(gameObj.away).then(function(responseAway) {
-        //                        console.log("home and away:");
-        //                        console.log(responseHome.data);
-        //                        console.log(responseAway.data);
-        //                        var gameDisplayObj = {
-        //                            "home": responseHome.data,
-        //                            "away": responseAway.data,
-        //                            "venue": gameObj.venue
-        //                        };
-        //                        $scope.resultsArr.push(gameDisplayObj);
-        //                    })
-        //                });
-        //
-        //            }
+        //        var gamesArr = response.data.games;
+        //        var gameIndex;
+        //        for (gameIndex in gamesArr) {
+        //            var homeTeam = gamesArr[gameIndex].home_team.market + " " + gamesArr[gameIndex].home_team.name;
+        //            var awayTeam = gamesArr[gameIndex].away_team.market + " " + gamesArr[gameIndex].away_team.name;
+        //            var gameDisplayObj = {
+        //                "home": homeTeam,
+        //                "away": awayTeam,
+        //                "homePoints": gamesArr[gameIndex].home_team.points,
+        //                "awayPoints": gamesArr[gameIndex].away_team.points
+        //            };
+        //            $scope.resultsArr.push(gameDisplayObj);
         //        }
         //    });
-
-        $http.get(nflFixtureUrl)
-            .then(function(response) {
-                $scope.resultsArr = [];
-                var gamesArr = response.data.games;
-                var gameIndex;
-                for (gameIndex in gamesArr) {
-                    var homeTeam = gamesArr[gameIndex].home_team.market + " " + gamesArr[gameIndex].home_team.name;
-                    var awayTeam = gamesArr[gameIndex].away_team.market + " " + gamesArr[gameIndex].away_team.name;
-                    var gameDisplayObj = {
-                        "home": homeTeam,
-                        "away": awayTeam,
-                        "homePoints": gamesArr[gameIndex].home_team.points,
-                        "awayPoints": gamesArr[gameIndex].away_team.points
-                    };
-                    $scope.resultsArr.push(gameDisplayObj);
-                }
-            });
 
         $scope.teamClicked = function(teamName) {
             $rootScope.clickedTeam = teamName;
