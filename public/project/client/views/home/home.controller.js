080062f2559bbe7d2218ce5114a3ef43552fa092
Sujith Narayan <sujithrp@ccs.neu.edu>
2016-04-19 18:53:36 -0400

    minor fixes - 3

--- public/project/client/views/home/home.controller.js
+++ public/project/client/views/home/home.controller.js
@@ -17,31 +17,31 @@
             });
         var nflFixtureUrl = "/sports/nfl-t1/2015/REG/4/boxscore.json?api_key=c9gmk2hsnccg8hcwhmfyj9uj";
 
-        var resultObj = {
-            "home": "home team fsdgfdsgsolnglng",
-            "away": "away team ffefbeklfbeufbh",
-            "homePoints": "45",
-            "awayPoints": "55"
-        };
-        $scope.resultsArr = [resultObj];
+        //var resultObj = {
+        //    "home": "home team fsdgfdsgsolnglng",
+        //    "away": "away team ffefbeklfbeufbh",
+        //    "homePoints": "45",
+        //    "awayPoints": "55"
+        //};
+        //$scope.resultsArr = [resultObj];
 
-        //$http.get(nflFixtureUrl)
-        //    .then(function(response) {
-        //        $scope.resultsArr = [];
-        //        var gamesArr = response.data.games;
-        //        var gameIndex;
-        //        for (gameIndex in gamesArr) {
-        //            var homeTeam = gamesArr[gameIndex].home_team.market + " " + gamesArr[gameIndex].home_team.name;
-        //            var awayTeam = gamesArr[gameIndex].away_team.market + " " + gamesArr[gameIndex].away_team.name;
-        //            var gameDisplayObj = {
-        //                "home": homeTeam,
-        //                "away": awayTeam,
-        //                "homePoints": gamesArr[gameIndex].home_team.points,
-        //                "awayPoints": gamesArr[gameIndex].away_team.points
-        //            };
-        //            $scope.resultsArr.push(gameDisplayObj);
-        //        }
-        //    });
+        $http.get(nflFixtureUrl)
+            .then(function(response) {
+                $scope.resultsArr = [];
+                var gamesArr = response.data.games;
+                var gameIndex;
+                for (gameIndex in gamesArr) {
+                    var homeTeam = gamesArr[gameIndex].home_team.market + " " + gamesArr[gameIndex].home_team.name;
+                    var awayTeam = gamesArr[gameIndex].away_team.market + " " + gamesArr[gameIndex].away_team.name;
+                    var gameDisplayObj = {
+                        "home": homeTeam,
+                        "away": awayTeam,
+                        "homePoints": gamesArr[gameIndex].home_team.points,
+                        "awayPoints": gamesArr[gameIndex].away_team.points
+                    };
+                    $scope.resultsArr.push(gameDisplayObj);
+                }
+            });
 
         $scope.teamClicked = function(teamName) {
             $rootScope.clickedTeam = teamName;
